d56302a24c878ea6fb413c05cb04017e7d09aa6e
[purrple durrple] <durrple@tuta.io>
2023-06-10 12:58:28 -0500

    fixed api key not working, added js css and html for movie info, added js to make movie search function work, fetching works and free movie poster works. also added if statements for any errors that could occur

--- assets/results.js
+++ assets/results.js
@@ -17,28 +17,45 @@ var getMovie = () => {
       fetch(url)
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(data);
-            console.log(data.Poster);
-            console.log(data.Title);
-            console.log(data.imdbRating);
-            console.log(data.Rated);
-            console.log(data.Year);
-            console.log(data.Runtime);
-            console.log(data.Genre);
-            console.log(data.Plot);
-            console.log(data.Actors);
-
-            result.innerHTML = `
+            //If movie exists in Database
+            if(data.Response == "True"){
+              result.innerHTML = `
                 <div class="info">
                     <img src=${data.Poster} class="poster">
                     <div>
                         <h2>${data.Title}</h2>
+                        <div class="rating">
+                            <img src="./assets/star.png">
+                            <h4>${data.imdbRating}</h4>
+                        </div>
+                        <div class="details">
+                            <span>${data.Rated}</span>
+                            <span>${data.Year}</span>
+                            <span>${data.Runtime}</span>
+                        </div>
+                        <div class="genre">
+                            <div>${data.Genre.split(",").join("</div><div>")}</div>
+                        </div>  
                     </div>
                 </div>
+                <h3>Plot:</h3>
+                <p>${data.Plot}</p>
+                <h3>Cast:</h3>
+                <p>${data.Actors}</p>
                 
             `;
+            }
+            //If movie does NOT exist in Database
+            else{
+                result.innerHTML=`<h3 class='msg'>${data.Error}</h3>`;
+            }
+        })
+        //If Error occurs
+        .catch(() => {
+            result.innerHTML=`<h3 class="msg">Error Occured</h3>`;
         });
     }
 };
 
+searchBtn.addEventListener("click", getMovie);
 window.addEventListener("load", getMovie);
